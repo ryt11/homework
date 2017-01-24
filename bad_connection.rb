495ac5c60276f215123abf5826121f1a172e3dfb
ryt11 <rileyt_2000@hotmail.com>
2017-01-24 10:09:40 -0600

    Initial commit

---
+++ bad_connection.rb
@@ -0,0 +1,28 @@
+puts "HELLO, THIS IS A GROCERY STORE!"
+goodbye_count = 0
+while true do
+  user_input = gets.chomp
+  input_array = user_input.split
+  if user_input == ""
+    puts "HELLO?!"
+  elsif user_input == "GOODBYE!" && goodbye_count == 0
+    puts "ANYTHING ELSE I CAN HELP WITH?"
+    goodbye_count += 1
+  elsif
+    user_input == "GOODBYE!" && goodbye_count == 1
+    puts "THANK YOU FOR CALLING!"
+    exit!
+
+  elsif user_input == user_input.upcase
+    puts "NO, THIS IS NOT A PET STORE"
+
+  elsif
+    input_array.each do |letter|
+    if letter == letter.downcase
+      puts "I AM HAVING A HARD TIME HEARING YOU!"
+      break
+      end
+    end
+  end
+end
+  
---
+++ pseudocode.rb
@@ -0,0 +1,20 @@
+# 1. The problem is solved when the program responds correctly
+# to all the specified input/output combinations.
+
+#2. The software will be initiated through the commandline and interact
+# with a user.
+
+#3. Have the program respond to no input with "HELLO?!"
+=begin
+4. a. If there is no input received the program should respond with "HELLO?!"
+   b. if the input is determined to have ANY lower-case letters the program
+      should respond with "I AM HAVING A HARD TIME HEARING YOU".
+   c. If the input is determined to have ALL up-case letters, the program
+      should respond with "NO, THIS IS NOT A PET STORE"
+   d. If the input is determined to be "GOODBYE!", and this is the first TIME
+       the program has received that input, the response should be "ANYTHING
+       ELSE I CAN HELP WITH?".
+   e. If the input is determined to be "GOODBYE!", and this is the second time
+       the program has received that input, the response should be "THANK YOU
+       FOR CALLING!", and the program should exit.
+=end
